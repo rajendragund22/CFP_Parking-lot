9b510697f8b0e933f726171a0c11c480894fab67
RajendraGund <rajendragund2015@gmail.com>
2021-11-11 07:58:20 +0530

    Added : Welcome message into the master

---
+++ src/test/java/com/bridgelabz/ParkingLotTest.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+
+import org.junit.Test;
+
+public class ParkingLotTest {
+    @Test
+    public void givenTestCasePass_ShouldReturnWelcome(){
+        System.out.println("Welcome into the Parking lot program");
+    }
+}
